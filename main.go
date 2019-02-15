5088fa84d9b9b2c37ea5ad7ec2b4ef5f964dc468
Geoff <picardrulez@gmail.com>
2019-02-15 13:10:31 -0500

    added manpage and version check

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
 )
 
+var VERSION = "v1.1"
+
 func main() {
+	versionFlag := flag.Bool("v", false, "display version")
+	flag.Parse()
+	if *versionFlag {
+		fmt.Println("runabout " + VERSION)
+		return
+	}
 	url := os.Args[1]
 	tre := url[:3]
 
@@ -15,7 +24,7 @@ func main() {
 	}
 
 	resp, err := http.Get(url)
-	if (err == nil) && (resp.StatusCode == 200) {
+	if (err == nil) && ((resp.StatusCode == 200) || (resp.StatusCode == 302)) {
 		fmt.Println("UP")
 	} else {
 		fmt.Println("DOWN")
